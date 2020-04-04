da2e8d4b24f230787f9ca8b6a28750eb966e9695
AmirHossein <amiryousefi98@gmail.com>
2020-04-04 17:08:04 +0430

    SecondPage v2

--- app/src/main/java/com/example/weather/ui/SecondPage.java
+++ app/src/main/java/com/example/weather/ui/SecondPage.java
@@ -10,5 +10,37 @@ public class SecondPage {
     private Activity secPageActivity;
     private ArrayList<Weather> weathers = new ArrayList<>();
 
+    private String latitude;
+    private String longitude;
 
+    public SecondPage(Activity activity){
+        this.secPageActivity = activity;
+
+    }
+
+    public void updaetWeather(ArrayList<Weather> myWheathers){
+        weathers.clear();
+        weathers.addAll(myWheathers);
+
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public Activity getSecPageActivity() {
+        return secPageActivity;
+    }
 }
