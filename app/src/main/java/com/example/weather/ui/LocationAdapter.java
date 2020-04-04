09a05404f8bfd213ba468c5b8b98b3320e960938
AmirHossein <amiryousefi98@gmail.com>
2020-04-04 16:08:23 +0430

    add item click listener

--- app/src/main/java/com/example/weather/ui/LocationAdapter.java
+++ app/src/main/java/com/example/weather/ui/LocationAdapter.java
@@ -7,10 +7,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.example.weather.MainActivity;
 import com.example.weather.R;
 import com.example.weather.api.Location;
 
@@ -39,10 +41,19 @@ public class LocationAdapter extends ArrayAdapter<Location> {
 
         System.out.println("^^^^^^^^" + location.getName());
 
+
+
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.first_listview, parent, false);
         }
-
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("location sekevted :::::::"+location.getName());
+                System.out.println("long :" + location.getLongitude());
+                System.out.println("Lant : " + location.getLatitude());
+            }
+        });
         TextView locationName = convertView.findViewById(R.id.location);
         locationName.setText(location.getName());
 
