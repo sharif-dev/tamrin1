18d2335b975e342823c84be02aafa8c4591063db
AmirHossein <amiryousefi98@gmail.com>
2020-04-04 16:06:39 +0430

    SecondPage

---
+++ app/src/main/java/com/example/weather/ui/SecondPage.java
@@ -0,0 +1,14 @@
+package com.example.weather.ui;
+
+import android.app.Activity;
+
+import com.example.weather.api.Weather;
+
+import java.util.ArrayList;
+
+public class SecondPage {
+    private Activity secPageActivity;
+    private ArrayList<Weather> weathers = new ArrayList<>();
+
+
+}
