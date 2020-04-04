a4e004563305318f71371eda636138c14aba6f97
AmirHossein <amiryousefi98@gmail.com>
2020-04-04 22:17:04 +0430

    replace first page layout with second page

--- app/src/main/java/com/example/weather/ui/LocationAdapter.java
+++ app/src/main/java/com/example/weather/ui/LocationAdapter.java
@@ -1,15 +1,13 @@
 package com.example.weather.ui;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.weather.MainActivity;
@@ -54,11 +52,25 @@ public class LocationAdapter extends ArrayAdapter<Location> {
                 System.out.println("long :" + location.getLongitude());
                 System.out.println("Lant : " + location.getLatitude());
                 //todo : new page2
-                MainActivity.secondPage.setLatitude(location.getLatitude());
-                MainActivity.secondPage.setLongitude(location.getLongitude());
+                //
+
+               MainActivity.secondPage.setLatitude(location.getLatitude());
+               MainActivity.secondPage.setLongitude(location.getLongitude());
+               MainActivity.firstPage.getActivity().setContentView(R.layout.second_page);
+               //todo:back button(test)
+               //Button button = MainActivity.secondPage.getSecPageActivity().findViewById(R.id.btn_back);
+               //button.setOnClickListener(new View.OnClickListener() {
+//                   @Override
+//                   public void onClick(View v) {
+//                       MainActivity.secondPage.getSecPageActivity().setContentView(R.layout.first_page);
+//                       FirstPage firstPage = new FirstPage(MainActivity.firstPage.getActivity());
+//                       firstPage.getActivity().setContentView(R.layout.first_page);
+//                   }
+//               });
 
                 APIThread apiThread = new APIThread(MainActivity.secondPage.getSecPageActivity() , "weather");
                 apiThread.start();
+
             }
         });
         TextView locationName = convertView.findViewById(R.id.location);
