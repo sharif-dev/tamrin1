75c11e13894e7d50cca90b9f933dcb65ad42f3a5
ghshenavar <ghshenavar@gmail.com>
2020-04-05 23:16:10 +0430

    Toast Class

---
+++ app/src/main/java/com/example/weather/ui/CustomToast.java
@@ -0,0 +1,14 @@
+package com.example.weather.ui;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.weather.R;
+
+public class CustomToast {
+    static int duration = Toast.LENGTH_LONG;
+    public CustomToast(Context context, int net){
+        Toast toast = Toast.makeText(context, net, duration);
+        toast.show();
+    }
+}
