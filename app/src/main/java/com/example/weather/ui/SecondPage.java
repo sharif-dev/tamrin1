47bd567c915c5b7045b3bd344c48fb99d84336c2
AmirHossein <amiryousefi98@gmail.com>
2020-04-05 23:17:36 +0430

    add list view

--- app/src/main/java/com/example/weather/ui/SecondPage.java
+++ app/src/main/java/com/example/weather/ui/SecondPage.java
@@ -21,20 +21,17 @@ public class SecondPage {
 
     public SecondPage(Activity activity){
         this.secPageActivity = activity;
-       // this.weatherAdapter = new WeatherAdapter(secPageActivity , weathers);
-        //this.weatherList = secPageActivity.findViewById(R.id.weather_listView);
-        //weatherList.setAdapter(weatherAdapter);
-
     }
 
     public void updaetWeather(ArrayList<Weather> myWheathers){
         weathers.clear();
         weathers.addAll(myWheathers);
+    }
 
-
-
-
-      //  weatherAdapter.notifyDataSetChanged();
+    public void updateUI(){
+       this.weatherAdapter = new WeatherAdapter(secPageActivity , weathers);
+       this.weatherList = secPageActivity.findViewById(R.id.weather_listView);
+       weatherList.setAdapter(weatherAdapter);
 
     }
 
