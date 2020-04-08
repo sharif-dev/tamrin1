ac3b5f47e77cf3ec5f42d0da430464cd27b436b9
baharkhd <baharkh127@gmail.com>
2020-04-09 00:22:01 +0430

    changed handler place for updating ui

--- app/src/main/java/com/example/weather/api/APIThread.java
+++ app/src/main/java/com/example/weather/api/APIThread.java
@@ -2,20 +2,13 @@ package com.example.weather.api;
 
 import android.app.Activity;
 import android.os.Handler;
-
-import android.os.Message;
-
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import com.example.weather.activities.FirstActivity;
 import com.example.weather.activities.WeatherParcelable;
-
 
 
 import java.util.ArrayList;
@@ -33,55 +26,49 @@ public class APIThread extends Thread {
 
         this.dataType = dataType;
 
-        this.handler = new Handler(new Handler.Callback() {
-            @Override
-            public boolean handleMessage(Message msg) {
+        this.handler = new Handler(msg -> {
 
-                if (msg.what == 0) { // update first page ui
-
-                    ArrayList<Location> locations = (ArrayList<Location>) msg.obj;
-
-                    FirstActivity.firstPage.getLoadFragment().endLoadingFragment();
-                    FirstActivity.firstPage.updateList(locations);
-
-
-                } else if (msg.what == 1) { // update second page ui
-                    //update second page list
-                    FirstActivity.firstPage.getLoadFragment().endLoadingFragment();
-                    ArrayList<WeatherParcelable> weathers = (ArrayList<WeatherParcelable>) msg.obj;
-                    FirstActivity.enterSecondPage(weathers, FirstActivity.firstPage.getActivity());
-
-                }
-
-                return true;
+            if (msg.what == 0) { // update first page ui
+                ArrayList<Location> locations = (ArrayList<Location>) msg.obj;
+                updateFirstPage(locations);
+            } else if (msg.what == 1) { // update second page ui
+                ArrayList<WeatherParcelable> weathers = (ArrayList<WeatherParcelable>) msg.obj;
+                updateSecondPage(weathers);
             }
+
+            return true;
+        });
+    }
+
+    public void updateFirstPage(ArrayList<Location> locations) {
+        handler.post(() -> {
+            FirstActivity.firstPage.getLoadFragment().endLoadingFragment();
+            FirstActivity.firstPage.updateList(locations);
+        });
+    }
+
+    public void updateSecondPage(ArrayList<WeatherParcelable> weathers) {
+        handler.post(() -> {
+            FirstActivity.firstPage.getLoadFragment().endLoadingFragment();
+            FirstActivity.enterSecondPage(weathers, FirstActivity.firstPage.getActivity());
         });
     }
 
     public void run() {
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
+        switch (dataType) {
+            case "location":
 
-                switch (dataType) {
-                    case "location":
+                String locationName = FirstActivity.firstPage.getEditTextLocation();
 
-                        String locationName = FirstActivity.firstPage.getEditTextLocation();
+                FirstActivity.firstPage.getLoadFragment().startLoadingFragment();
 
-                        FirstActivity.firstPage.getLoadFragment().startLoadingFragment();
-
-                        getLocation(locationName);
-                        break;
-                    case "weather":
-                        FirstActivity.firstPage.getLoadFragment().startLoadingFragment();
-                        getWeather(latitude , longitude);
-                        break;
-                }
-//                getWeather("51.67917", "32.65139");
-
-
-            }
-        });
+                getLocation(locationName);
+                break;
+            case "weather":
+                FirstActivity.firstPage.getLoadFragment().startLoadingFragment();
+                getWeather(latitude, longitude);
+                break;
+        }
     }
 
 
@@ -90,38 +77,19 @@ public class APIThread extends Thread {
                 "_token=" + FirstActivity.mapbox_token;
 
 
-        sendRequest(new VolleyCallback() {
-            @Override
-            public void onSuccess(String result) {
-                Location.processLocationsRes(result, handler);
-            }
-        }, url);
+        sendRequest(result -> Location.processLocationsRes(result, handler), url);
     }
 
     public void getWeather(String latitude, String longitude) {
         String url = FirstActivity.darksky_url + FirstActivity.darksky_secret_key + "/" +
                 latitude + "," + longitude;
 
-        sendRequest(new VolleyCallback() {
-            @Override
-            public void onSuccess(String result) {
-                WeatherParcelable.processWeatherRes(result, handler);
-            }
-        }, url);
+        sendRequest(result -> WeatherParcelable.processWeatherRes(result, handler), url);
     }
 
     public void sendRequest(final VolleyCallback callback, String url) {
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                callback.onSuccess(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                url, response -> callback.onSuccess(response), Throwable::printStackTrace);
 
         requestQueue.add(stringRequest);
     }
